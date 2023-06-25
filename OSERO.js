23736b41ce4f1a58e085e1f83763c8a4a2b9214a
s_masui <shintarou.masui@altrobo.co.jp>
2023-06-25 10:11:29 +0900

    Neat arrangement

--- OSERO.js
+++ OSERO.js
@@ -25,15 +25,20 @@ function putStone(x, y, color){
 
 // クリックした座標に石を置く
 function putStoneAt(event){
-
 	// HTMLの中にCANVASがあるので、CANVASのどの位置をクリックしたか
-
+ 	// 調整するために、CANVASとHTMLの位置関係から補正しています
   var rect = event.target.getBoundingClientRect();
   x = event.clientX - rect.left;
   y = event.clientY - rect.top;
   console.log({x, y})
+  // 20 + 40n に配置されるようにすれば良い
+  // % は 余りを取得する演算子 
+  // fx1. 158なら38 (158-38+20=140) 
+  // fx2. 170なら１０ (170-10+20=180) 
+  x = x - x % 40 + 20 // キリが良い箇所に配置されるようにx座標を補正
+  y = y - y % 40 + 20 // キリが良い箇所に配置されるようにy座標を補正
   putStone(x, y, "white")
-  // 下だと、クリック位置とずれる
+  // 参考：下記の実装だと、クリック位置とずれる
   // console.log({event.clientX, event.clientY})
   // putStone(event.clientX, event.clientY, "white")
 }
