593ba4ea65e50701b56c85f3371b19d290ed57f0
s_masui <shintarou.masui@altrobo.co.jp>
2023-06-29 14:12:48 +0900

    配置と切り替え機能

--- OSERO.js
+++ OSERO.js
@@ -1,9 +1,10 @@
 var canvas = document.getElementById("reversi")
-var ctx = canvas.getContext("2d")
+var displayPlayer = document.getElementById("player")
+var log = document.getElementById("log")
 
-//初期配置
+var ctx = canvas.getContext("2d")
 var playBoard = [
-	[0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0],
   [0,0,0,0,0,0,0,0],
   [0,0,0,0,0,0,0,0],
   [0,0,0,0,0,0,0,0],
@@ -12,55 +13,79 @@ var playBoard = [
   [0,0,0,0,0,0,0,0],
   [0,0,0,0,0,0,0,0],
 ]
+var player = 1
+displayPlayer.innerText = "white"
 
 ctx.fillStyle = "black"
 
-for (let col = 0; col < 8; col++) {
-  ctx.beginPath(); // スタートパス
-  ctx.moveTo(40 * col, 0); 
-  ctx.lineTo(40 * col, 320); // 線を描く
-  ctx.stroke();
-}
-for (let row = 0; row < 8; row++) {
-  ctx.beginPath(); // スタートパス
-  ctx.moveTo(0, 40 * row); 
-  ctx.lineTo(320, 40 * row); // 線を描く 
-  ctx.stroke();
-}
-
-function putStone(x, y, color){
-	ctx.fillStyle = color;
-  ctx.beginPath();
-  ctx.arc(x, y, 16, 0, 2 * Math.PI);
-  ctx.fill();
-}
-
 // クリックした座標に石を置く
-function putStoneAt(event){
-	// HTMLの中にCANVASがあるので、CANVASのどの位置をクリックしたか
- 	
+function onClickPutStone(event){
+  // HTMLの中にCANVASがあるので、CANVASのどの位置をクリックしたか
+ 
   var rect = event.target.getBoundingClientRect();
   x = event.clientX - rect.left;
   y = event.clientY - rect.top;
   x = x - x % 40 + 20 // キリが良い箇所に配置されるようにx座標を補正
   y = y - y % 40 + 20 // キリが良い箇所に配置されるようにy座標を補正
-  putStone(x, y, "white");
   
   // どこに石を置いたか記憶する
   var posX = (x-20) / 40
   var posY = (y-20) / 40
-  playBoard[posY][posX] = 1 
-  // 1:white 2:black とする
+
+  putStone(posX, posY, player);
   console.log(playBoard)
 }
 
+function changePlayer(){
+  if(player == 1){
+  	player = 2
+    displayPlayer.innerText = "black"
+  }else{
+  	player = 1
+    displayPlayer.innerText = "white"
+  }
+}
 // 初期配置を行う関数
 function initialize(){
-  putStone(140, 140, "white")
-  putStone(180, 140, "black")
-  putStone(140, 180, "black")
-  putStone(180, 180, "white")
+  putStone(3, 4, 1)
+  putStone(4, 4, 2)
+  putStone(3, 3, 2)
+  putStone(4, 3, 1)
+}
+// 座標を指定しして石を置く
+function putStone(x, y, stone){
+  if(stone == 1){
+    color = "white"
+  }else{
+  	color = "black"
+  }
+  
+  ctx.fillStyle = color;
+  ctx.beginPath();
+  ctx.arc(40*x + 20, 40*y + 20, 16, 0, 2 * Math.PI);
+  ctx.fill();
+
+  playBoard[x][y] = 1 // 1:white 2:black とする
+}
+// マス目を引く処理
+function drawPlayBoard () {
+  // 縦に線を引く
+  for (let col = 0; col < 8; col++) {
+    ctx.beginPath(); // Start a new path
+    ctx.moveTo(40 * col, 0); // Move the pen to
+    ctx.lineTo(40 * col, 320); // Draw a line to
+    ctx.stroke();
+  }
+  // 横に線を引く
+  for (let row = 0; row < 8; row++) {
+    ctx.beginPath(); // Start a new path
+    ctx.moveTo(0, 40 * row); // Move the pen to
+    ctx.lineTo(320, 40 * row); // Draw a line to 
+    ctx.stroke();
+  }
 }
 
-// 初期配置を実装
+// 盤面を描く
+drawPlayBoard()
+// 初期配置
 initialize()
